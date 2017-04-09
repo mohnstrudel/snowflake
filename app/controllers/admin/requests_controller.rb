63f9263e36f1fbd1ee41509b58424aaac50c4ff3
Anton Kostin <schnibba@gmail.com>
2017-04-09 20:54:31 +0300

    Further tweaks to default display.

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,9 +1,27 @@
 class Admin::RequestsController < AdminController
-	def index
+	
+  before_action :find_request, only: [:show, :destroy]
+
+  def index
 		@requests = Request.order('created_at DESC')
 	end
 
 	def show
 		@request = Request.find(params[:id])
 	end
+
+  def destroy
+    if @request.destroy
+      redirect_to admin_requests_path, method: :get
+      flash[:success] = "Успешно удалено"
+    else
+      render :index
+    end
+  end
+
+  private 
+
+  def find_request
+    @request = Request.find(params[:id])
+  end
 end
